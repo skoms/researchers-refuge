b70b4c1e868d17c3021e2af35703f7d75ffef2cd
skoms <45171368+skoms@users.noreply.github.com>
2021-08-09 12:44:00 +0200

    add styling to stats for userProfile

--- client/src/components/userProfile/UserProfile.js
+++ client/src/components/userProfile/UserProfile.js
@@ -5,19 +5,37 @@ const UserProfile = () => {
     <div className='user-profile-page'>
       <div className="user-profile-div">
         <div className="user-profile-info-header">
-          <img src="https://via.placeholder.com/1000x150" alt="header" className="header-img" />
+        <div className="header-img-div">
+          <img src="https://via.placeholder.com/1000x150" alt="header"  className="header-img"/>
           <img src="https://via.placeholder.com/120x120" alt="profilepic" className="profile-pic" />
           <button className='button-primary'>Follow</button>
+        </div>
+          
           <div className="name-and-occupation">
             <h2 className="full-name">Kari Nordmann</h2>
             <p className="occupation">Unemployed</p>
           </div>
           <div className="stats">
-            <p>Most active field: n</p>
-            <p>Articles: n</p>
-            <p>Credits: n</p>
-            <p>Followers: n</p>
-            <p>Following: n</p>
+            <div className="stat">
+              <p className="title">Most active field:</p>
+              <p className="data">n</p>
+            </div>
+            <div className="stat">
+              <p className="title">Articles:</p>
+              <p className="data">n</p>
+            </div>
+            <div className="stat">
+              <p className="title">Credits:</p>
+              <p className="data">n</p>
+            </div>
+            <div className="stat">
+              <p className="title">Followers:</p>
+              <p className="data">n</p>
+            </div>
+            <div className="stat">
+              <p className="title">Following:</p>
+              <p className="data">n</p>
+            </div>
           </div>
         </div>
       </div>
