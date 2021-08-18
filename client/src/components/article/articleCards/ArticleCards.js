d35bba6e1ca82d44f1bc72df34f8c115320dadfd
skoms <45171368+skoms@users.noreply.github.com>
2021-08-18 16:51:57 +0200

    add onLoad basic fetch for articles for main page

--- client/src/components/article/articleCards/ArticleCards.js
+++ client/src/components/article/articleCards/ArticleCards.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 
-import ArticleCard from './articleCard/ArticleCard'
-import tempCards from "../../../data/articles";
+import ArticleCard from './articleCard/ArticleCard';
+import { Context } from '../../../Context';
 
 const ArticleCards = (props) => {
+  const [articles, setArticles] = useState([  ]);
+  const history = useHistory();
+  const [didLoad, setDidLoad] = useState(false);
+  const context = useContext(Context);
+
+  useEffect(()=> {
+    const getArticles = async () => {
+      await context.data.getArticles()
+        .then( res => {
+          switch (res.status) {
+            case 200:
+              setArticles(res.articles);
+              console.log(res.articles);
+              break;
+            case 500:
+              history.push('/error');
+              break;
+            default:
+              console.log(`STATUS: ${res.status}, ERRORS: ${res.errors}`);
+              break;
+          }
+        })
+        .catch(err => {
+          console.error(err);
+          history.push('/error');
+        })
+    }
+    if (!didLoad) {
+      setDidLoad(true);
+      getArticles();
+    }
+  }, [didLoad, context.data, history])
+
   window.addEventListener('scroll', e => {
     let scrollbarTimeout;
     if ( e.target.tagName !== undefined 
@@ -18,13 +52,14 @@ const ArticleCards = (props) => {
   return (
     <div className='article-cards'>
       {
-        tempCards.map( card => 
+        articles.map( card => 
           <ArticleCard 
             id={card.id}
             key={card.id}
             title={card.title}
             topic={card.topic}
-            author={card.author}
+            author={`${card.User.firstName} ${card.User.lastName}`}
+            authorId={card.userId}
             intro={card.intro}
           />
         )
