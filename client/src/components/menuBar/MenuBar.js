281ddf0323a2de321461744b49775e1e13048c4d
skoms <45171368+skoms@users.noreply.github.com>
2021-08-18 16:23:23 +0200

    add a way to display and store menu-bar filter selection

--- client/src/components/menuBar/MenuBar.js
+++ client/src/components/menuBar/MenuBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const MenuBar = () => {
+const MenuBar = props => {
+  const { select } = props;
   return (
     <div className='menu-bar'>
       <div>
-        <button>Popular</button>
-        <button>New</button>
-        <button>Top</button>
-        <button>By Topic</button>
+        <button onClick={(e) => select(e)}>Popular</button>
+        <button onClick={select}>New</button>
+        <button onClick={select}>Top</button>
+        <button onClick={select}>By Topic</button>
       </div>
       <a href="/write-article">
         <button>Write New Article</button>
