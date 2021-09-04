83b865fe68ec7fd5dc12960efe69de7a48827a09
skoms <45171368+skoms@users.noreply.github.com>
2021-09-04 13:54:19 +0200

    add stying and base layout for the edit profile module

--- client/src/components/user/userProfile/editProfile/EditProfile.js
+++ client/src/components/user/userProfile/editProfile/EditProfile.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux';
+import { selectDarkModeOn } from '../../../darkmodeButton/darkModeButtonSlice';
 import TopicSelect from '../../../topicSelect/TopicSelect';
 
 const EditProfile = () => {
+  const darkModeOn = useSelector(selectDarkModeOn);
   const tempUser = {
     title: '',
     intro: '',
@@ -20,13 +22,14 @@ const EditProfile = () => {
     <div id='edit-profile-div'>
       <div className="edit-header"> 
         <h2>Edit Profile</h2>
-        <button>
-          <img src="https://img.icons8.com/ios-filled/20/000000/x.png" alt='Exit button'/>
+        <button className='exit-button'>
+          <img src={`https://img.icons8.com/ios-filled/16/${ darkModeOn ? 'E8F7FF' : '1A3861'}/x.png`} alt='Exit button'/>
         </button>
       </div>
       <div className="edit-sidebar">
-        <p className="profile">My Profile</p>
-        <p className="account">My Account</p>
+        <button><p className="profile">My Profile</p></button>
+        <hr />
+        <button><p className="account">My Account</p></button>
       </div>
       <div className="edit-main">
         <form className="edit-user-form" onSubmit={submit}>
@@ -45,7 +48,7 @@ const EditProfile = () => {
           </div>
           <div className="form-input mostActiveField" id='mostActiveField-input-div'>
             <TopicSelect use='ArticleForm' />
-            <label htmlFor="mostActiveField">Topic: </label>
+            <label htmlFor="mostActiveField">Most Active Field: </label>
           </div>
           <div className='form-input bio' id='bio-input-div'>
             <input id="bio" name="bio" type="text" value={ tempUser.bio || '' } onChange={onChangeHandler} placeholder='Uses Markdown formatting'/>
